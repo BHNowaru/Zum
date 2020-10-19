57121392b45b692ad556386cb4958a707fb9b48d
BHNowaru <zackyboy135@gmail.com>
2020-10-19 00:19:55 -0700

    Flesh out request testing

--- client/index.js
+++
@@ -1,9 +0,0 @@
-const HTTP = require("http");
-
-document.getElementById("requestbutton").addEventListener("click", async () => {
-    var testRequest = new Request()
-    var testHeaders = new Headers();
-    testHeaders.set("Authorization", "ligmoid");
-    
-    const Response = await fetch(testRequest);
-})
