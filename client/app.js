98ddf59c78025cdc0ae9608acce7d6c510f370c8
BHNowaru <zackyboy135@gmail.com>
2020-10-18 23:05:09 -0700

    General layout commit.

---
+++ client/app.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const Window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    });
+    Window.open("./index.html");
+}
+
+app.whenReady().then(createWindow);
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length <= 0) {
+        createWindow();
+    }
+})
