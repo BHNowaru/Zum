deed3c9924450bf1d69f7a421531b6145e7dc370
BHNowaru <zackyboy135@gmail.com>
2020-10-19 13:32:17 -0700

    Connect prototype

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+
+//initialize particles using particlesjs (not particles.js. two different things.)
 const Particles = require("particlesjs")
 window.onload = function() {
     Particles.init({
@@ -5,3 +7,48 @@ window.onload = function() {
         color: "#F0EDEE",
     })
 }
+setTimeout(() => {
+    $("#connect").removeAttr("disabled")
+}, 3000)
+//allow connecting to socket
+const io = require("socket.io-client");
+var Reconnecting = false;
+const Socket = io("135.180.72.85:777", {
+    reconnection: true,
+    reconnectionAttempts: 3,
+    reconnectionDelayMax: 3500,
+    reconnectionDelay: 3000,
+    autoConnect: false,  
+    query: {
+        auth: "ligmoid"
+    }
+})
+
+    
+Socket.on("connect_error", async () => {
+    console.log("Failed to connect to Socket.");
+})
+
+Socket.on("connect", () => {
+    console.log("Successfully connected to Socket.")
+})
+
+Socket.on("disconnected", () => {
+    console.log("Disconnected from Socket.")
+})
+
+Socket.on("reconnecting", () => {
+    console.log("Reconnecting...")
+})
+
+Socket.on("reconnect_failed", () => {
+    console.log("Reconnect failed.")
+})
+
+$("#connect").on("click", () => {
+    if (Reconnecting || Socket.connected) {
+        return;
+    }
+
+    Socket.open();
+})
